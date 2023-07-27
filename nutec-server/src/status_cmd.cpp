456aa753ee7196c2b72d448575b3f77dc8df090e
espressjo <jonathan@astro.umontreal.ca>
2023-07-27 10:36:53 -0400

    ...

--- nutec-server/src/status_cmd.cpp
+++ nutec-server/src/status_cmd.cpp
@@ -10,30 +10,37 @@ void get_status_code(instHandle *handle,cmd *cc)
     if (readRegister_32(handle,"0xa0",&v0xa0,true)!=0)
     {
         printf("Unable to read r0xa0\n");
-        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED);
-        return ;
+
     }
-    if (readRegister(handle,"0xa0",&check,true)!=0)
+    else 
     {
-        printf("Unable to read r0xa0 (string)\n");
-        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED);
-        return ;
+        for (int i=0;i<=31;i++)
+            {
+                if ( (static_cast<uint32_t>(pow(2,i)) & v0xa0) == static_cast<uint32_t>(pow(2,i)) )
+                {
+                    message+=R0XAO(i)+sep;
+                }
+            }
+        
     }
-    cout<<"[0xa0] : "<<v0xa0<<" - "<<check<<endl;
+
     
     if (readRegister_32(handle,"0xc9",&v0xc9,true)!=0)
     {
         printf("Unable to read 0xc9\n");
-        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED);
-        return ;
+
     }
-    if (readRegister(handle,"0xc9",&check,true)!=0)
+    else 
     {
-        printf("Unable to read 0xc9 (string)\n");
-        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED);
-        return ;
+        for (int i=0;i<=15;i++)
+        {
+            if ( (static_cast<uint32_t>(pow(2,i)) & v0xc9) == static_cast<uint32_t>(pow(2,i))) 
+            {
+                message+=R0XC9(i)+sep;
+            }
+        }
+        
     }
-    cout<<"[0xc9] : "<<v0xc9<<" - "<<check<<endl;
     
     if (readRegister_32(handle,"0xa4",&v0xa4,true)!=0)
     {
@@ -41,38 +48,17 @@ void get_status_code(instHandle *handle,cmd *cc)
         sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED);
         return ;
     }
-    if (readRegister(handle,"0xa4",&check,true)!=0)
+    else 
     {
-        printf("Unable to read 0xa4 (string)\n");
-        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED);
-        return ;
-    }
-    cout<<"[0xa4] : "<<v0xa4<<" - "<<check<<endl;
-    
-    for (int i=0;i<=31;i++)
-    {
-        if ( (static_cast<uint32_t>(pow(2,i)) & v0xa0) == static_cast<uint32_t>(pow(2,i)) )
+        for (int i=0;i<=18;i++)
         {
-            message+=R0XAO(i)+sep;
-        }
+            if ( (static_cast<uint32_t>(pow(2,i)) & v0xa4) == static_cast<uint32_t>(pow(2,i)) )
+            {
+                message+=R0XA4(i)+sep;
+            }
+        }    
+        
     }
-    
-    for (int i=0;i<=15;i++)
-    {
-        if ( (static_cast<uint32_t>(pow(2,i)) & v0xc9) == static_cast<uint32_t>(pow(2,i))) 
-        {
-            message+=R0XC9(i)+sep;
-        }
-    }
-    for (int i=0;i<=18;i++)
-    {
-        if ( (static_cast<uint32_t>(pow(2,i)) & v0xa4) == static_cast<uint32_t>(pow(2,i)) )
-        {
-            message+=R0XA4(i)+sep;
-        }
-    }
-    
-    
     
     sndMsg(cc->sockfd,message.substr(0,message.length()-1));
     return ;
