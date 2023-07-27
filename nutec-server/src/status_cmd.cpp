18c83e9349c8e5eee3b3484da8686782a4a9eb95
espressjo <jonathan@astro.umontreal.ca>
2023-07-27 10:31:24 -0400

    ...

--- nutec-server/src/status_cmd.cpp
+++ nutec-server/src/status_cmd.cpp
@@ -10,36 +10,48 @@ void get_status_code(instHandle *handle,cmd *cc)
     if (readRegister_32(handle,"0xa0",&v0xa0,true)!=0)
     {
         printf("Unable to read r0xa0\n");
+        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED)
+        return ;
     }
     if (readRegister(handle,"0xa0",&check,true)!=0)
     {
         printf("Unable to read r0xa0 (string)\n");
+        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED)
+        return ;
     }
     cout<<"[0xa0] : "<<v0xa0<<" - "<<check<<endl;
     
     if (readRegister_32(handle,"0xc9",&v0xc9,true)!=0)
     {
         printf("Unable to read 0xc9\n");
+        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED)
+        return ;
     }
     if (readRegister(handle,"0xc9",&check,true)!=0)
     {
         printf("Unable to read 0xc9 (string)\n");
+        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED)
+        return ;
     }
     cout<<"[0xc9] : "<<v0xc9<<" - "<<check<<endl;
     
     if (readRegister_32(handle,"0xa4",&v0xa4,true)!=0)
     {
         printf("Unable to read 0xa4\n");
+        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED)
+        return ;
     }
     if (readRegister(handle,"0xa4",&check,true)!=0)
     {
         printf("Unable to read 0xa4 (string)\n");
+        sndMsg(cc->sockfd,"error",uicsCMD_ERR_NOT_SUPPORTED)
+        return ;
     }
     cout<<"[0xa4] : "<<v0xa4<<" - "<<check<<endl;
     
     for (int i=0;i<=31;i++)
     {
-        if ( (static_cast<uint32_t>(pow(2,i)) & v0xa4) == static_cast<uint32_t>(pow(2,i)) )
+        if ( (static_cast<uint32_t>(pow(2,i)) & v0xa0) == static_cast<uint32_t>(pow(2,i)) )
         {
             message+=R0XAO(i)+sep;
         }
