d51ec14beb3aa402db9bc339804a84f9bb8a2e37
espressjo <jonathan@astro.umontreal.ca>
2023-08-13 23:35:49 -0400

    ...

--- python/script/astrometry.py
+++ python/script/astrometry.py
@@ -7,11 +7,11 @@ Created on Fri Aug 11 14:18:01 2023
 """
 
 from guideCam import coarseCam
-from sys import argv
 from arguments import get_arg_int
 
 max_stars = get_arg_int("--max-stars")
 if max_stars==-1:
     max_stars=15
-with coarseCam(hwsimul=True) as cam:
-    A = cam.astrometry(max_star=max_stars,sim_image="/home/hicibas-clone/test-loop40.fits")#should update the astrometry telemetry
+with coarseCam(hwsimul=False) as cam:
+    #A = cam.astrometry(max_star=max_stars,sim_image="/home/hicibas-clone/test-loop40.fits")#should update the astrometry telemetry
+    A = cam.astrometry(max_star=max_stars)#should update the astrometry telemetry
